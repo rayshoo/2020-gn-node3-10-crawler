d57ac4cf47794b044dd8834428f43695c7de18dd
dfnk5516 <rayshoo@naver.com>
2020-07-12 12:15:10 +0900

    cheerio done

--- index.js
+++ index.js
@@ -8,18 +8,25 @@ const csv = fs.readFileSync('./data/coupang.csv');
 const sendData = parse(csv.toString('utf-8'));
 // console.log(sendData);
 const crawler = async () => {
-  let result, txt, price, img, $;
+  let result,
+    title,
+    price,
+    img,
+    wrTxt = '',
+    $;
   for (let v of sendData) {
     result = await axios.get(v[1]);
     if (result.status === 200) {
       $ = cheerio.load(result.data);
-      txt = $('h2.prod-buy-header__title').text();
+      title = $('h2.prod-buy-header__title').text();
       price = $('.total-price strong').text().trim();
       price = price.substr(0, price.indexOf('원')) + '원';
       img = $('img.prod-image__detail').attr('src').trim();
-      console.log(txt, price, img, '\n');
+      console.log(title, price, img, '\n');
+      wrTxt += `${v[0]},${v[1]},${title},${price},${img}\n`;
     }
     // console.log(result);
+    fs.writeFileSync('./result/coupang.csv', wrTxt);
   }
 };
 
